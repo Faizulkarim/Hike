c1051a506372c31eef32acabf298dd026d800b11
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-01 20:31:07 +0600

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Md Faizul karim on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
